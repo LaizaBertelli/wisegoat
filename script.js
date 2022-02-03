3342c4b5d1e6efff3701af6ed6f3c6a849e62a94
LaizaBertelli <laizabertelli@hotmail.com>
2022-02-02 23:51:15 -0300

    feat: implements function to 'change' pages

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const API_URL = 'https://api.adviceslip.com/advice';
 const askAdviceButton = document.getElementById('ask-advice-btn');
 
+function removeMainPageElements() {
+    const header = document.querySelector('.header');
+    header.removeChild(askAdviceButton);
+    
+    const main = document.querySelector('main');
+    main.classList.remove('home');
+    main.classList.add('advice');
+
+    const imgContainer = document.querySelector('.image-container');
+    const goatImg = document.querySelector('.goat-image');
+    imgContainer.removeChild(goatImg);
+    
+}
+
 async function handleClick() {
+    removeMainPageElements();
     const response = await fetch(API_URL);
     const { slip } = await response.json();
     console.log(slip.advice);
